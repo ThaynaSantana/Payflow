cca41e2f3a0192cd299983c85d9e3c6fda0f0887
Thayna <thaynas.s502@gmail.com>
2022-05-25 11:45:49 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:payflow/app_widget.dart';
+import 'package:payflow/modules/home/home_page.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+
+void main(List<String> args) {
+  runApp(AppFirebase());
+}
+
+class AppFirebase extends StatefulWidget {
+  const AppFirebase({Key? key}) : super(key: key);
+
+  @override
+  State<AppFirebase> createState() => _AppFirebaseState();
+}
+
+class _AppFirebaseState extends State<AppFirebase> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Material(
+            child: Center(
+              child: Text(
+                'Nao foi possivel inicializar o Firebase!',
+                textDirection: TextDirection.ltr,
+              ),
+            ),
+          );
+        } else if (snapshot.connectionState == ConnectionState.done) {
+          return AppWidget();
+        } else {
+          return Material(
+            child: Center(child: CircularProgressIndicator()),
+          );
+        }
+      },
+    );
+  }
+}
