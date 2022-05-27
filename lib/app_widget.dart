1381ba747208f8d200223972f602b673a0d45f62
Thayna <thaynas.s502@gmail.com>
2022-05-26 21:24:12 -0300

    updates mods

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/modules/home/home_page.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
+import 'package:payflow/shared/models/barcode_scanner/barcode_scanner_page.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 
 import 'modules/splash/login/login_page.dart';
@@ -12,12 +13,15 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'PayFlow',
-      theme: ThemeData(primaryColor: AppColors.primary),
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        primaryColor: AppColors.primary),
       initialRoute: '/splash',
       routes: {
         '/splash': (context) => const SplashPage(),
         '/home': (context) => const HomePage(),
-        '/login': (context) => const LoginPage()
+        '/login': (context) => const LoginPage(),
+        '/barcode_scanner': (context) => const BarcodeScannerPage()
       }
     );
   }
